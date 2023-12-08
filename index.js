88f50bdf875ec1c6794c164563a7c805965e3609
Michal Kluba <m.klubaa@gmail.com>
2023-12-08 14:38:40 +0100

    added task url to the top

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /**
+ * //https://leetcode.com/problems/two-sum/description/
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -6,8 +7,6 @@
 
 const nums = [3, 2, 3];
 const target = 6;
-
-//https://leetcode.com/problems/two-sum/description/
 
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
