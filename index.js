85d2ee19168cd563305bd2e8cd694b6f23a4dcca
Michal Kluba <m.klubaa@gmail.com>
2023-12-14 22:20:23 +0100

    Performance improvement

--- index.js
+++ index.js
@@ -5,19 +5,9 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  let charCounter = 0;
-  for (var i = 0; i < haystack.length; i++) {
-    for (let j = 0; j < needle.length; j++) {
-      console.log(`Checking ${haystack[i + charCounter]} == ${needle[j]}`);
-      if (haystack[i + charCounter] == needle[j]) {
-        charCounter++;
-        if (charCounter == needle.length) {
-          return i;
-        }
-      } else {
-        charCounter = 0;
-        break;
-      }
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack.substring(i, needle.length + i) == needle) {
+      return i;
     }
   }
   return -1;
