bb5dd4eef32b67d61287c74f36c3b33329dcdd5c
Michal Kluba <m.klubaa@gmail.com>
2023-12-18 19:30:16 +0100

    135. Candy

--- index.js
+++ index.js
@@ -3,4 +3,21 @@
  * @param {number[]} ratings
  * @return {number}
  */
-var candy = function (ratings) {};
+var candy = function (ratings) {
+  let counter = new Array();
+  for (let i = 0; i < ratings.length; i++) {
+    counter[i] = 1;
+  }
+  console.log(counter);
+
+  for (let i = 1; i < ratings.length; i++) {
+    if (ratings[i] > ratings[i - 1]) counter[i] = counter[i - 1] + 1;
+  }
+
+  for (let i = ratings.length - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1])
+      counter[i] = Math.max(counter[i], counter[i + 1] + 1);
+  }
+
+  return counter.reduce((a, b) => a + b, 0);
+};
